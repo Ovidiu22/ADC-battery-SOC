8435cd0d792d416c83cc6420672f3cd87f7d2d3f
Ovidiu22 <75970114+Ovidiu22@users.noreply.github.com>
2022-01-20 19:44:56 +0100

    Full implementation
    - TODO: schematic and description of setup

---
+++ src/SOC.h
@@ -0,0 +1,8 @@
+
+#ifndef DIAGNOSTICS_H_
+#define DIAGNOSTICS_H_
+
+/* Prototype functions */
+uint8_t getBatterySOC( uint8_t * );
+
+#endif /* DIAGNOSTICS_H_ */
--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
+/*******************************/
+/*** Created by Ovidiu Sabau ***/
+/***	20th January 2022	 ***/
+/*******************************/
 
 #include <asf.h>
+#include "SOC.h"
+#include "LCD.h"
+
 
 int main (void)
 {
-	//
+	/* Local variables */
+	uint8_t soc = 0;
+	uint8_t diag = 0;
+	initLCD();
+	
+	while (1)
+	{
+		soc = getBatterySOC(&diag);
+		displayLCD_main(1, "Battery soc: ", soc, " %");	
+		displayLCD_main(2, "Battery DTC: ", diag, "NONE");	// Diagnostic i.e. low battery
+		_delay_ms(1000);
+	}
 }
